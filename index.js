bebeaefe70d879c6c2023f9b70963bccb919c593
Angel Lau <alau7693@gmail.com>
2020-06-24 20:25:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 function dwarfRollCall(dwarves) {
+  let string = '';
+  for (let i = 0; i < dwarves.length; i++) {
+    string += (`${i +1}. ${dwarves[i]} `)
+  } return string
 }
 
-function summonCaptainPlanet(planeteerCalls){
+
+function summonCaptainPlanet(planeteerCalls) {
+  let array = [];
+  for (let i = 0; i < planeteerCalls.length; i++) {
+    array.push(`${planeteerCalls[i].toUpperCase()}!`)
+  } return array
 }
+
+// function longPlaneteerCalls(words) {
+//   for (let i = 0; i < words.length; i++) {
+//     if (words[i].length > 4) {
+//       return true
+//     } else {
+//       return false
+//     }
+//   }
+// }
 
 function longPlaneteerCalls(words) {
+  for (let i = 0; i < words.length; i++) {
+    return (words[i].length > 4) 
+  }
 }
 
 function findTheCheese (foods) {
+  let cheese = ["cheddar", "gouda", "camembert"]
+  for (let i = 0; i < foods.length; i++) {
+    let cheeseIdx = cheese.indexOf(foods[i])
+  }
 }
